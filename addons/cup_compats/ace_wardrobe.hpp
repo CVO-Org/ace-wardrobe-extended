1211731ca03f92ec721e4cb634c32a728a351474
OverlordZorn <dennisAzorn@gmail.com>
2025-10-24 11:17:20 +0200

    init

---
+++ addons/cup_compats/ace_wardrobe.hpp
@@ -0,0 +1,180 @@
+#include "\z\ace\addons\wardrobe\script_macros_wardrobe.hpp"
+class ace_wardrobe {
+    // Remove unused imported base classes once done
+    class ace_wardrobe_base;
+    class ace_wardrobe_base_H_visor_up;
+    class ace_wardrobe_base_H_visor_down;
+    class ace_wardrobe_base_H_goggles_on;
+    class ace_wardrobe_base_H_goggles_off;
+    class ace_wardrobe_base_H_mask_on;
+    class ace_wardrobe_base_H_mask_off;
+    class ace_wardrobe_base_U_sleeves_up;
+    class ace_wardrobe_base_U_sleeves_down;
+    class ace_wardrobe_base_U_gloves_on;
+    class ace_wardrobe_base_U_gloves_off;
+    class ace_wardrobe_base_U_jacket_open;
+    class ace_wardrobe_base_U_jacket_closed;
+
+
+    // ToDo MACRO
+    class CUP_U_O_RUS_Soldier_VKPO_Desert_1: EGVAR(wardrobe,base) {
+        class modifiableTo {
+            class CUP_U_O_RUS_Soldier_VKPO_Desert_2 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Desert_3 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Desert_4 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Rolled_Desert_1 {};
+        };
+    };
+    class CUP_U_O_RUS_Soldier_VKPO_Desert_2: EGVAR(wardrobe,base) {
+        class modifiableTo {
+            class CUP_U_O_RUS_Soldier_VKPO_Desert_1 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Rolled_Desert_2 {};
+        };
+    };
+    class CUP_U_O_RUS_Soldier_VKPO_Desert_3: EGVAR(wardrobe,base) {
+        class modifiableTo {
+            class CUP_U_O_RUS_Soldier_VKPO_Desert_1 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Rolled_Desert_3 {};
+        };
+    };
+    class CUP_U_O_RUS_Soldier_VKPO_Desert_4: EGVAR(wardrobe,base) {
+        class modifiableTo {
+            class CUP_U_O_RUS_Soldier_VKPO_Desert_1 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Rolled_Desert_4 {};
+        };
+    };
+    class CUP_U_O_RUS_Soldier_VKPO_Rolled_Desert_1: EGVAR(wardrobe,base) {
+        class modifiableTo {
+            class CUP_U_O_RUS_Soldier_VKPO_Desert_1 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Rolled_Desert_2 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Rolled_Desert_3 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Rolled_Desert_4 {};
+        };
+    };
+    class CUP_U_O_RUS_Soldier_VKPO_Rolled_Desert_2: EGVAR(wardrobe,base) {
+        class modifiableTo {
+            class CUP_U_O_RUS_Soldier_VKPO_Desert_1 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Desert_2 {};
+        };
+    };
+    class CUP_U_O_RUS_Soldier_VKPO_Rolled_Desert_3: EGVAR(wardrobe,base) {
+        class modifiableTo {
+            class CUP_U_O_RUS_Soldier_VKPO_Desert_1 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Desert_3 {};
+        };
+    };
+    class CUP_U_O_RUS_Soldier_VKPO_Rolled_Desert_4: EGVAR(wardrobe,base) {
+        class modifiableTo {
+            class CUP_U_O_RUS_Soldier_VKPO_Desert_1 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Desert_4 {};
+        };
+    };
+
+    class CUP_U_O_RUS_Soldier_VKPO_Summer_1: EGVAR(wardrobe,base) {
+        class modifiableTo {
+            class CUP_U_O_RUS_Soldier_VKPO_Summer_2 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Summer_3 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Summer_4 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Rolled_Summer_1 {};
+        };
+    };
+    class CUP_U_O_RUS_Soldier_VKPO_Summer_2: EGVAR(wardrobe,base) {
+        class modifiableTo {
+            class CUP_U_O_RUS_Soldier_VKPO_Summer_1 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Rolled_Summer_2 {};
+        };
+    };
+    class CUP_U_O_RUS_Soldier_VKPO_Summer_3: EGVAR(wardrobe,base) {
+        class modifiableTo {
+            class CUP_U_O_RUS_Soldier_VKPO_Summer_1 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Rolled_Summer_3 {};
+        };
+    };
+    class CUP_U_O_RUS_Soldier_VKPO_Summer_4: EGVAR(wardrobe,base) {
+        class modifiableTo {
+            class CUP_U_O_RUS_Soldier_VKPO_Summer_1 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Rolled_Summer_4 {};
+        };
+    };
+    class CUP_U_O_RUS_Soldier_VKPO_Rolled_Summer_1: EGVAR(wardrobe,base) {
+        class modifiableTo {
+            class CUP_U_O_RUS_Soldier_VKPO_Summer_1 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Rolled_Summer_2 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Rolled_Summer_3 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Rolled_Summer_4 {};
+        };
+    };
+    class CUP_U_O_RUS_Soldier_VKPO_Rolled_Summer_2: EGVAR(wardrobe,base) {
+        class modifiableTo {
+            class CUP_U_O_RUS_Soldier_VKPO_Summer_1 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Summer_2 {};
+        };
+    };
+    class CUP_U_O_RUS_Soldier_VKPO_Rolled_Summer_3: EGVAR(wardrobe,base) {
+        class modifiableTo {
+            class CUP_U_O_RUS_Soldier_VKPO_Summer_1 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Summer_3 {};
+        };
+    };
+    class CUP_U_O_RUS_Soldier_VKPO_Rolled_Summer_4: EGVAR(wardrobe,base) {
+        class modifiableTo {
+            class CUP_U_O_RUS_Soldier_VKPO_Summer_1 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Summer_4 {};
+        };
+    };
+
+    class CUP_U_O_RUS_Soldier_VKPO_Winter_1: EGVAR(wardrobe,base) {
+        class modifiableTo {
+            class CUP_U_O_RUS_Soldier_VKPO_Winter_2 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Winter_3 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Winter_4 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Rolled_Winter_1 {};
+        };
+    };
+    class CUP_U_O_RUS_Soldier_VKPO_Winter_2: EGVAR(wardrobe,base) {
+        class modifiableTo {
+            class CUP_U_O_RUS_Soldier_VKPO_Winter_1 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Rolled_Winter_2 {};
+        };
+    };
+    class CUP_U_O_RUS_Soldier_VKPO_Winter_3: EGVAR(wardrobe,base) {
+        class modifiableTo {
+            class CUP_U_O_RUS_Soldier_VKPO_Winter_1 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Rolled_Winter_3 {};
+        };
+    };
+    class CUP_U_O_RUS_Soldier_VKPO_Winter_4: EGVAR(wardrobe,base) {
+        class modifiableTo {
+            class CUP_U_O_RUS_Soldier_VKPO_Winter_1 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Rolled_Winter_4 {};
+        };
+    };
+    class CUP_U_O_RUS_Soldier_VKPO_Rolled_Winter_1: EGVAR(wardrobe,base) {
+        class modifiableTo {
+            class CUP_U_O_RUS_Soldier_VKPO_Winter_1 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Rolled_Winter_2 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Rolled_Winter_3 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Rolled_Winter_4 {};
+        };
+    };
+    class CUP_U_O_RUS_Soldier_VKPO_Rolled_Winter_2: EGVAR(wardrobe,base) {
+        class modifiableTo {
+            class CUP_U_O_RUS_Soldier_VKPO_Winter_1 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Winter_2 {};
+        };
+    };
+    class CUP_U_O_RUS_Soldier_VKPO_Rolled_Winter_3: EGVAR(wardrobe,base) {
+        class modifiableTo {
+            class CUP_U_O_RUS_Soldier_VKPO_Winter_1 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Winter_3 {};
+        };
+    };
+    class CUP_U_O_RUS_Soldier_VKPO_Rolled_Winter_4: EGVAR(wardrobe,base) {
+        class modifiableTo {
+            class CUP_U_O_RUS_Soldier_VKPO_Winter_1 {};
+            class CUP_U_O_RUS_Soldier_VKPO_Winter_4 {};
+        };
+    };
+
+
+};
---
+++ addons/cup_compats/script_component.hpp
@@ -0,0 +1,9 @@
+#define COMPONENT cup_compats
+
+
+// This is being used for the Addon's Name and can be "My Addon Template Framework"
+#define COMPONENT_BEAUTIFIED CUP Compats
+
+
+#include "\x\awx\addons\main\script_mod.hpp"
+#include "\x\awx\addons\main\script_macros.hpp"
