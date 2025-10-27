0406be43a586d5ef227cc1117d609666145cb2d3
Andx <andx667@protonmail.com>
2025-10-27 16:59:25 +0100

    initial commit

---
+++ addons/usp_uniforms_compats/ace_wardrobe.hpp
@@ -0,0 +1,14 @@
+#include "\z\ace\addons\wardrobe\script_macros_wardrobe.hpp"
+
+class ace_wardrobe {
+    // Remove unused imported base classes once done
+    class ace_wardrobe_base;
+    class ace_wardrobe_base_U_sleeves_up;
+    class ace_wardrobe_base_U_sleeves_down;
+    class ace_wardrobe_base_U_gloves_on;
+    class ace_wardrobe_base_U_gloves_off;
+    class ace_wardrobe_base_U_jacket_open;
+    class ace_wardrobe_base_U_jacket_closed;
+
+    #include "ace_wardrobe_uniforms.hpp"
+};
