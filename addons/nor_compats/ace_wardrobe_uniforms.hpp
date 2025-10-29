845b39b7c2fa18c0bcbda0cd07e7628416eaca1c
Andy <andx667@protonmail.com>
2025-10-29 16:33:39 +0100

    improve base classes usage

--- addons/nor_compats/ace_wardrobe_uniforms.hpp
+++ addons/nor_compats/ace_wardrobe_uniforms.hpp
@@ -4,13 +4,13 @@ class NOR_Combat_Uniform: ace_wardrobe_base {
         class NOR_Combat_Uniform_Rolled {};
     };
 };
-class NOR_Combat_Uniform_Gloves: ace_wardrobe_base {
+class NOR_Combat_Uniform_Gloves: ace_wardrobe_base_U_gloves_on {
     class modifiableTo {
         class NOR_Combat_Uniform {};
         class NOR_Combat_Uniform_Gloves_Rolled {};
     };
 };
-class NOR_Combat_Uniform_Rolled: ace_wardrobe_base {
+class NOR_Combat_Uniform_Rolled: ace_wardrobe_base_U_sleeves_up {
     class modifiableTo {
         class NOR_Combat_Uniform {};
         class NOR_Combat_Uniform_Gloves_Rolled {};
