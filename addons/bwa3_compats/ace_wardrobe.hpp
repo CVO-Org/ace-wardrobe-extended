cfcab1d7d4cae21e20ff821fb3e20848413c0d04
Andy <andx667@protonmail.com>
2025-10-29 16:35:08 +0100

    improve base classes, add fnl

--- addons/bwa3_compats/ace_wardrobe.hpp
+++ addons/bwa3_compats/ace_wardrobe.hpp
@@ -1,7 +1,6 @@
 #include "\z\ace\addons\wardrobe\script_macros_wardrobe.hpp"
 
 class ace_wardrobe {
-    class ace_wardrobe_base;
     class ace_wardrobe_base_U_sleeves_up;
     class ace_wardrobe_base_U_sleeves_down;
 
