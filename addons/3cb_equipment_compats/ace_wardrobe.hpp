7cbefa98b43d422d6878a1379d3a355a17baba93
Andx <andx667@protonmail.com>
2025-10-30 19:42:08 +0100

    cleanup

--- addons/3cb_equipment_compats/ace_wardrobe.hpp
+++ addons/3cb_equipment_compats/ace_wardrobe.hpp
@@ -1,7 +1,7 @@
 #include "\z\ace\addons\wardrobe\script_macros_wardrobe.hpp"
 
 class ace_wardrobe {
-    // Remove unused imported base classes once done
+
     class ace_wardrobe_base;
     class ace_wardrobe_base_U_sleeves_up;
     class ace_wardrobe_base_U_sleeves_down;
--- addons/3cb_equipment_compats/script_component.hpp
+++ addons/3cb_equipment_compats/script_component.hpp
@@ -1,6 +1,4 @@
 #define COMPONENT 3cb_equipment_compats
-
-// This is being used for the Addon's Name and can be "My Addon Template Framework"
 #define COMPONENT_BEAUTIFIED 3CB Equipment Compats
 
 #include "\x\awx\addons\main\script_mod.hpp"
