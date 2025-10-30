5324934ad37aa7a20f8f1cf4f5e2e33dbcc6151d
Andx <andx667@protonmail.com>
2025-10-30 19:41:04 +0100

    add headgear

--- addons/3cb_equipment_compats/ace_wardrobe.hpp
+++ addons/3cb_equipment_compats/ace_wardrobe.hpp
@@ -5,6 +5,8 @@ class ace_wardrobe {
     class ace_wardrobe_base;
     class ace_wardrobe_base_U_sleeves_up;
     class ace_wardrobe_base_U_sleeves_down;
+    class ace_wardrobe_base_H_goggles_on;
+    class ace_wardrobe_base_H_goggles_off;
 
     #include "ace_wardrobe_uniforms.hpp"
     #include "ace_wardrobe_headgear.hpp"
