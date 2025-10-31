1cda12b59c3c447de780833160a9b7c01cdd855a
Andx <andx667@protonmail.com>
2025-10-31 13:00:45 +0100

    add helmets

--- addons/3cb_factions_compat/ace_wardrobe_headgear.hpp
+++ addons/3cb_factions_compat/ace_wardrobe_headgear.hpp
@@ -37,6 +37,9 @@ HELMET_GOGGLES(UK3CB_UN_B_H_6b27m_Cov,UK3CB_UN_B_H_6b27m_ESS_Cov,rhs_ess_black);
 HELMET_GOGGLES(UK3CB_ABP_B_H_6b27m_UCC,UK3CB_ABP_B_H_6b27m_ess_UCC,rhs_ess_black);
 HELMET_GOGGLES(UK3CB_ANA_B_H_6b27m_WDL,UK3CB_ANA_B_H_6b27m_ESS_WDL,rhs_ess_black);
 
+HELMET_GOGGLES(UK3CB_ARD_B_H_Crew_Cap,UK3CB_ARD_B_H_Crew_Cap_Ess,rhsusf_oakley_goggles_blk);
+HELMET_GOGGLES(UK3CB_ARD_B_UK3CB_H_Crew_CapH_Crew_Cap,UK3CB_H_Crew_Cap_Ess,rhsusf_oakley_goggles_blk);
+
 #define HELMET_ESS_BALACLAVA(HelmetBase,HelmetGoggles,HelmetBalaclava,HelmetCombo,Goggles,Balaclava)\
 class HelmetBase: ace_wardrobe_base {\
     class modifiableTo {\
@@ -70,3 +73,79 @@ HELMET_ESS_BALACLAVA(UK3CB_ANP_B_H_6b27m_BLK,UK3CB_ANP_B_H_6b27m_ess_BLK,UK3CB_A
 HELMET_ESS_BALACLAVA(UK3CB_ANP_B_H_6b27m_GREY,UK3CB_ANP_B_H_6b27m_ess_GREY,UK3CB_ANP_B_H_6b27m_bala_GREY,UK3CB_ANP_B_H_6b27m_ess_bala_GREY,rhs_ess_black,rhs_balaclava);
 HELMET_ESS_BALACLAVA(UK3CB_CSAT_W_O_H_6b47_ATACS,UK3CB_CSAT_W_O_H_6b47_ess_ATACS,UK3CB_CSAT_W_O_H_6b47_bala_ATACS,UK3CB_CSAT_W_O_H_6b47_ess_bala_ATACS,rhs_ess_black,rhs_balaclava);
 HELMET_ESS_BALACLAVA(UK3CB_CSAT_S_O_H_6b47_WIN,UK3CB_CSAT_S_O_H_6b47_ess_WIN,UK3CB_CSAT_S_O_H_6b47_bala_WIN,UK3CB_CSAT_S_O_H_6b47_ess_bala_WIN,rhs_ess_black,UK3CB_G_Balaclava_WIN_ALT);
+
+//In theory you could also swap between the 3 facewrap variantes and the 3 neckwrap variants of each color
+HELMET_MASK(UK3CB_H_Turban_Facewrap_01_Blk,UK3CB_H_Turban_Neckwrap_01_Blk);
+HELMET_MASK(UK3CB_H_Turban_Facewrap_02_Blk,UK3CB_H_Turban_Neckwrap_02_Blk);
+HELMET_MASK(UK3CB_H_Turban_Facewrap_03_Blk,UK3CB_H_Turban_Neckwrap_03_Blk);
+
+HELMET_MASK(UK3CB_H_Turban_Facewrap_01_Blu,UK3CB_H_Turban_Neckwrap_01_Blu);
+HELMET_MASK(UK3CB_H_Turban_Facewrap_02_Blu,UK3CB_H_Turban_Neckwrap_02_Blu);
+HELMET_MASK(UK3CB_H_Turban_Facewrap_03_Blu,UK3CB_H_Turban_Neckwrap_03_Blu);
+
+HELMET_MASK(UK3CB_H_Turban_Facewrap_01_Oli,UK3CB_H_Turban_Neckwrap_01_Oli);
+HELMET_MASK(UK3CB_H_Turban_Facewrap_02_Oli,UK3CB_H_Turban_Neckwrap_02_Oli);
+HELMET_MASK(UK3CB_H_Turban_Facewrap_03_Oli,UK3CB_H_Turban_Neckwrap_03_Oli);
+
+HELMET_MASK(UK3CB_H_Turban_Facewrap_01_Red,UK3CB_H_Turban_Neckwrap_01_Red);
+HELMET_MASK(UK3CB_H_Turban_Facewrap_02_Red,UK3CB_H_Turban_Neckwrap_02_Red);
+HELMET_MASK(UK3CB_H_Turban_Facewrap_03_Red,UK3CB_H_Turban_Neckwrap_03_Red);
+
+HELMET_MASK(UK3CB_H_Turban_Facewrap_01_Tan,UK3CB_H_Turban_Neckwrap_01_Tan);
+HELMET_MASK(UK3CB_H_Turban_Facewrap_02_Tan,UK3CB_H_Turban_Neckwrap_02_Tan);
+HELMET_MASK(UK3CB_H_Turban_Facewrap_03_Red,UK3CB_H_Turban_Neckwrap_03_Tan);
+
+HELMET_MASK(UK3CB_H_Turban_Facewrap_01_Trq,UK3CB_H_Turban_Neckwrap_01_Trq);
+HELMET_MASK(UK3CB_H_Turban_Facewrap_02_Trq,UK3CB_H_Turban_Neckwrap_02_Trq);
+HELMET_MASK(UK3CB_H_Turban_Facewrap_03_Trq,UK3CB_H_Turban_Neckwrap_03_Trq);
+
+HELMET_MASK(UK3CB_H_Turban_Facewrap_01_Whi,UK3CB_H_Turban_Neckwrap_01_Whi);
+HELMET_MASK(UK3CB_H_Turban_Facewrap_02_Whi,UK3CB_H_Turban_Neckwrap_02_Whi);
+HELMET_MASK(UK3CB_H_Turban_Facewrap_03_Whi,UK3CB_H_Turban_Neckwrap_02_Whi);
+
+HELMET_MASK(UK3CB_H_Turban_Facewrap_01_Yel,UK3CB_H_Turban_Neckwrap_01_Yel);
+HELMET_MASK(UK3CB_H_Turban_Facewrap_02_Yel,UK3CB_H_Turban_Neckwrap_02_Yel);
+HELMET_MASK(UK3CB_H_Turban_Facewrap_03_Yel,UK3CB_H_Turban_Neckwrap_03_Yel);
+
+//In theory, you could have the ear protection and add it to the Cap
+BASE_PAIR(UK3CB_H_Cap_Back_ION_BLK,UK3CB_H_Cap_Headset_ION_BLK);
+BASE_PAIR(UK3CB_H_Radio_Cap_Back_ION_BLK,UK3CB_H_Radio_Cap_ION_BLK);
+BASE_PAIR(UK3CB_H_Radio_Cap_Back_ION_BLK_ALT,UK3CB_H_Radio_Cap_ION_BLK_ALT);
+BASE_PAIR(UK3CB_H_Cap_Back_ION_DES,UK3CB_H_Cap_ION_DES);
+BASE_PAIR(UK3CB_H_Radio_Cap_Back_ION_DES,UK3CB_H_Radio_Cap_ION_DES);
+BASE_PAIR(UK3CB_H_Radio_Cap_Back_ION_DES_ALT,UK3CB_H_Radio_Cap_ION_DES_ALT);
+BASE_PAIR(UK3CB_H_Cap_Back_ION_OLI,UK3CB_H_Cap_ION_OLI);
+BASE_PAIR(UK3CB_H_Radio_Cap_Back_ION_OLI,UK3CB_H_Radio_Cap_ION_OLI);
+BASE_PAIR(UK3CB_H_Radio_Cap_Back_ION_OLI_ALT,UK3CB_H_Radio_Cap_ION_OLI_ALT);
+BASE_PAIR(UK3CB_H_Cap_Back_ION_WHI,UK3CB_H_Cap_ION_WHI);
+BASE_PAIR(UK3CB_H_Radio_Cap_Back_ION_WHI,UK3CB_H_Radio_Cap_ION_WHI);
+BASE_PAIR(UK3CB_H_Radio_Cap_Back_ION_WHI_ALT,UK3CB_H_Radio_Cap_ION_WHI_ALT);
+
+class UK3CB_LDF_B_H_HB97_GEO:ace_wardrobe_base_H_goggles_off {
+    class modifiableTo {
+        class UK3CB_LDF_B_H_HB97_ESS_GEO {};
+    };
+};
+class UK3CB_LDF_B_H_HB97_ESS_GEO: ace_wardrobe_base_H_goggles_on {
+    class modifiableTo {
+        class UK3CB_LDF_B_H_HB97_ESS_GEO {};
+        class UK3CB_LDF_B_H_HB97_ESS_Cover_GEO {};
+    };
+    components[] = {QUOTE(rhs_ess_black)};
+};
+class UK3CB_LDF_B_H_HB97_ESS_Cover_GEO: ace_wardrobe_base_H_goggles_on {
+    class modifiableTo {
+        class UK3CB_LDF_B_H_HB97_ESS_GEO {};
+        class UK3CB_LDF_B_H_HB97_ESS_Cover_GEO {};
+    };
+    components[] = {QUOTE(rhs_ess_black)};
+};
+
+
+HELMET_VISOR(UK3CB_H_Pilot_Helmet,UK3CB_H_Crew_Helmet);
+BASE_PAIR(UK3CB_TKM_O_H_Turban_01_1,UK3CB_TKM_O_H_Turban_02_1);
+BASE_PAIR(UK3CB_TKM_I_H_Turban_01_1,UK3CB_TKM_I_H_Turban_02_1);
+HELMET_VISOR(UK3CB_ARD_B_H_zsh7a_mike,UK3CB_ARD_B_H_zsh7a_mike_alt);
+HELMET_VISOR(UK3CB_ARD_B_H_zsh7a,UK3CB_ARD_B_H_zsh7a_alt);
+HELMET_VISOR(UK3CB_TKA_O_H_zsh7a_mike_Des,UK3CB_TKA_O_H_zsh7a_mike_Des_alt);
+HELMET_VISOR(UK3CB_TKA_O_H_zsh7a_Des,UK3CB_TKA_O_H_zsh7a_Des_alt);
