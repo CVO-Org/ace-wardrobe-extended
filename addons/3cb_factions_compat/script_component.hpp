e150949eb32e7328635e448ba567aa23f92d030a
Andx <andx667@protonmail.com>
2025-10-31 11:42:29 +0100

    add uniforms

---
+++ addons/3cb_factions_compat/script_component.hpp
@@ -0,0 +1,5 @@
+#define COMPONENT 3cb_factions_compat
+#define COMPONENT_BEAUTIFIED 3CB Factions Compat
+
+#include "\x\awx\addons\main\script_mod.hpp"
+#include "\x\awx\addons\main\script_macros.hpp"
