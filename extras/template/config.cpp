68dcf55dff71eeee00ed24702c142a6ff26f9b3e
Andx <andx667@protonmail.com>
2025-11-01 09:19:42 +0100

    add uniforms and helmets

--- extras/template/config.cpp
+++ extras/template/config.cpp
@@ -7,7 +7,7 @@ class CfgPatches {
         name = ADDON_NAME;
         author = "$STR_mod_author";
         authors[] = {"You!"};
-        
+
         url = "$STR_mod_URL";
 
         VERSION_CONFIG;
@@ -18,11 +18,11 @@ class CfgPatches {
 
         // Required addons, used for setting load order.
         // When any of the addons is missing, pop-up warning will appear when launching the game.
-        requiredAddons[] = {QPVAR(main), "mod_dependency"};
+        requiredAddons[] = {QPVAR(main), "Flex_CUP_SPA_Equipment"};
 
         // Optional. If this is 1, if any of requiredAddons[] entry is missing in your game the entire config will be ignored and return no error (but in rpt) so useful to make a compat Mod (Since Arma 3 2.14)
         skipWhenMissingDependencies = 1;
-        
+
         // List of objects (CfgVehicles classes) contained in the addon. Important also for Zeus content (units and groups)
         units[] = {};
 
