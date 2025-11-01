b49d5906242aa8c5216d3a9b971201e323180e6c
Andx <andx667@protonmail.com>
2025-11-01 08:57:03 +0100

    more cleanup

--- addons/cup_pol_compat/ace_wardrobe.hpp
+++ addons/cup_pol_compat/ace_wardrobe.hpp
@@ -3,9 +3,7 @@
 class ace_wardrobe {
     class ace_wardrobe_base;
     class ace_wardrobe_base_U_sleeves_up;
-    class ace_wardrobe_base_U_sleeves_down;
     class ace_wardrobe_base_U_gloves_on;
-    class ace_wardrobe_base_U_gloves_off;
 
     #include "ace_wardrobe_uniforms.hpp"
 };
