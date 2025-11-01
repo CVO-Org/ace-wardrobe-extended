363b7e17294cd2487a04e0ca28b8961ac6847269
Andx <andx667@protonmail.com>
2025-11-01 10:08:15 +0100

    add

---
+++ addons/cup_ukr_compat/script_component.hpp
@@ -0,0 +1,5 @@
+#define COMPONENT cup_ukr_compat
+#define COMPONENT_BEAUTIFIED CUP Armed Forces of Ukraine Compat
+
+#include "\x\awx\addons\main\script_mod.hpp"
+#include "\x\awx\addons\main\script_macros.hpp"
