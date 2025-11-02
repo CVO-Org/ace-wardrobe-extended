f1e3f38376d5305b39b8e825a4caf94989c28512
Andx <andx667@protonmail.com>
2025-11-02 17:30:07 +0100

    imitial commit

---
+++ addons/vnx_compat/script_component.hpp
@@ -0,0 +1,5 @@
+#define COMPONENT vnx_compat
+#define COMPONENT_BEAUTIFIED S.O.G. Nickel and Steel Compat
+
+#include "\x\awx\addons\main\script_mod.hpp"
+#include "\x\awx\addons\main\script_macros.hpp"
