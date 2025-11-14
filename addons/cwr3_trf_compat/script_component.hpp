be6e3f0ce480311270ec8beab99abb63a5802225
Andx <andx667@protonmail.com>
2025-11-14 08:57:43 +0100

    cleanup

--- addons/cwr3_trf_compat/script_component.hpp
+++
@@ -1,5 +0,0 @@
-#define COMPONENT cwr3_trf_compat
-#define COMPONENT_BEAUTIFIED Cold War Rearmed III Tonali Defense Force Compat
-
-#include "\x\awx\addons\main\script_mod.hpp"
-#include "\x\awx\addons\main\script_macros.hpp"
