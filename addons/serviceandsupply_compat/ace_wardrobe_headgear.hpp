5c153a501ea04b22657453636dbe8ceec79b12a5
Andx <andx667@protonmail.com>
2025-11-14 12:25:59 +0100

    fix config errors

--- addons/serviceandsupply_compat/ace_wardrobe_headgear.hpp
+++ addons/serviceandsupply_compat/ace_wardrobe_headgear.hpp
@@ -1,6 +1,6 @@
 HELMET_GOGGLES_COMPONENT(H_Simc_M1_erdl_ns,H_Simc_M1_erdl_swdg,G_SWDG);
 HELMET_GOGGLES_COMPONENT(H_Simc_M1_bitch_flip_ns,H_Simc_M1_bitch_flip_swdg,G_SWDG);
-HELMET_GOGGLES_COMPONENT(H_Simc_M1_bitch_ns,H_Simc_M1_bitch_swdg);
+HELMET_GOGGLES_COMPONENT(H_Simc_M1_bitch_ns,H_Simc_M1_bitch_swdg,G_SWDG);
 
 class H_Simc_Hat_Patrol_od7: ace_wardrobe_base {
     class modifiableTo {
@@ -21,7 +21,7 @@ class H_Simc_Hat_Patrol_od7_swdg_rear: ace_wardrobe_base_H_goggles_off {
     components[] = {"G_SWDG"};
 };
 
-HELMET_GOGGLES2(H_Simc_M1C_bitch_ns,H_Simc_M1C_bitch_swdg,G_SWDG);
+HELMET_GOGGLES_COMPONENT(H_Simc_M1C_bitch_ns,H_Simc_M1C_bitch_swdg,G_SWDG);
 
 class H_Simc_MC_hat_1:ace_wardrobe_base {
     class modifiableTo {
