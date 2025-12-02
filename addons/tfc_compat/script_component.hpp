001bb1cec7db087f5bc25d6e6e96d817fee00cd4
Andx <andx667@protonmail.com>
2025-12-02 18:24:40 +0100

    rename

---
+++ addons/tfc_compat/script_component.hpp
@@ -0,0 +1,4 @@
+#define COMPONENT ca_compat
+#define COMPONENT_BEAUTIFIED Canadian Armed Forces Mod Compat
+#include "\x\awx\addons\main\script_mod.hpp"
+#include "\x\awx\addons\main\script_macros.hpp"
