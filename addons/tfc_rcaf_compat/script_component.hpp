c905378fbfd4502da41a43984f88f0e397ab1ac0
Andx <andx667@protonmail.com>
2025-12-02 18:26:38 +0100

    cleanup

--- addons/tfc_rcaf_compat/script_component.hpp
+++
@@ -1,4 +0,0 @@
-#define COMPONENT ca_compat
-#define COMPONENT_BEAUTIFIED Mod Compat - Canadian Armed Forces
-#include "\x\awx\addons\main\script_mod.hpp"
-#include "\x\awx\addons\main\script_macros.hpp"
