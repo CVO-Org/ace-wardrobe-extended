6936575d2753cfd633be7f4e3248fd0108842fd5
Andx <17100110+Andx667@users.noreply.github.com>
2025-12-02 17:01:51 +0100

    Update addons/tfc_rcaf_compat/script_component.hpp
    Co-authored-by: OverlordZorn <56258612+OverlordZorn@users.noreply.github.com>

--- addons/tfc_rcaf_compat/script_component.hpp
+++ addons/tfc_rcaf_compat/script_component.hpp
@@ -1,4 +1,4 @@
 #define COMPONENT ca_compat
-#define COMPONENT_BEAUTIFIED Canadian Armed Forces Mod Compat
+#define COMPONENT_BEAUTIFIED Mod Compat - Canadian Armed Forces
 #include "\x\awx\addons\main\script_mod.hpp"
 #include "\x\awx\addons\main\script_macros.hpp"
