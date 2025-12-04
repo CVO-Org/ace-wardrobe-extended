c7fddbb9603000cbc74ee905c991c2020bfb0718
OverlordZorn <dennisAzorn@gmail.com>
2025-12-04 20:56:02 +0100

    upt template

--- extras/template/script_component.hpp
+++ extras/template/script_component.hpp
@@ -1,5 +1,5 @@
 #define COMPONENT addonname
-#define COMPONENT_BEAUTIFIED Addon Name
+#define COMPONENT_BEAUTIFIED Mod Compat - Addon Name // alternative: CDLC Compat - CDLC Name
 
 #include "\x\awx\addons\main\script_mod.hpp"
 #include "\x\awx\addons\main\script_macros.hpp"
