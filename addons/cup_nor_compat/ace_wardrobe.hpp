9d925e276abe7bfa9f5ffada6eb7e5aaeef5ad93
Andy <andx667@protonmail.com>
2026-02-19 14:32:49 +0100

    cleanup

--- addons/cup_nor_compat/ace_wardrobe.hpp
+++ addons/cup_nor_compat/ace_wardrobe.hpp
@@ -4,8 +4,6 @@ class ace_wardrobe {
     class ACEWARDROBE(base);
     class ACEWARDROBE(base_U_sleeves_up);
     class ACEWARDROBE(base_U_sleeves_down);
-    class ACEWARDROBE(base_U_gloves_on);
-    class ACEWARDROBE(base_U_gloves_off);
 
     #include "ace_wardrobe_uniforms.hpp"
     #include "ace_wardrobe_helmets.hpp"
--- addons/main/script_version.hpp
+++ addons/main/script_version.hpp
@@ -1,7 +1,7 @@
 #define MAJOR 0
 #define MINOR 0
 #define PATCH 0
-#define BUILD 63
+#define BUILD 66
 
 
 // #define VERSION MACROS
