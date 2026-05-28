74197201bd8404b3d3ce3e4473bcc78635336883
Andy <andx667@protonmail.com>
2026-05-28 09:43:19 +0200

    remove cwr3_baf_compat addon files

--- addons/cwr3_baf_compat/script_component.hpp
+++
@@ -1,5 +0,0 @@
-#define COMPONENT cwr3_baf_compat
-#define COMPONENT_BEAUTIFIED Mod Compat - CWR3 British Armed Forces
-
-#include "\x\awx\addons\main\script_mod.hpp"
-#include "\x\awx\addons\main\script_macros.hpp"
