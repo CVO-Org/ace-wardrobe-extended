8813b067a00a4c12bae915f52d1b4bdd205c9922
Andx <andx667@protonmail.com>
2026-08-19 22:41:06 +0200

    add blackdogs compat

---
+++ addons/blackdogs_compat/script_component.hpp
@@ -0,0 +1,5 @@
+#define COMPONENT blackdogs_compat
+#define COMPONENT_BEAUTIFIED Mod Compat - The Black Dogs of Burnham
+
+#include "\x\awx\addons\main\script_mod.hpp"
+#include "\x\awx\addons\main\script_macros.hpp"
